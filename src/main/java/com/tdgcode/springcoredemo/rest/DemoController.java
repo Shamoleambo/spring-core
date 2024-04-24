1ed39a342774ca3229faba757ce5bd0ae72054a9
Tiago Diniz Gomes <tidgomes@gmail.com>
2024-04-24 11:25:10 -0300

    use lazy initialization in the coach implementation classes

--- src/main/java/com/tdgcode/springcoredemo/rest/DemoController.java
+++ src/main/java/com/tdgcode/springcoredemo/rest/DemoController.java
@@ -1,6 +1,7 @@
 package com.tdgcode.springcoredemo.rest;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,9 +11,10 @@ import com.tdgcode.springcoredemo.common.Coach;
 public class DemoController {
 
 	private Coach myCoach;
-	
+
 	@Autowired
-	public DemoController(Coach theCoach) {
+	public DemoController(@Qualifier("cricketCoach") Coach theCoach) {
+		System.out.println("In constructor: " + getClass().getSimpleName());
 		this.myCoach = theCoach;
 	}
 
