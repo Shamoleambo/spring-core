2798a1900a7847b2cc41c77857e5b915b2ead0e3
Tiago Diniz Gomes <tidgomes@gmail.com>
2024-04-24 10:47:59 -0300

    set the TrackCoach as the Primary Coach implementation

--- src/main/java/com/tdgcode/springcoredemo/rest/DemoController.java
+++ src/main/java/com/tdgcode/springcoredemo/rest/DemoController.java
@@ -1,7 +1,6 @@
 package com.tdgcode.springcoredemo.rest;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,7 +12,7 @@ public class DemoController {
 	private Coach myCoach;
 	
 	@Autowired
-	public DemoController(@Qualifier("cricketCoach") Coach theCoach) {
+	public DemoController(Coach theCoach) {
 		this.myCoach = theCoach;
 	}
 
